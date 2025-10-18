344c0304fe7ffe7215a624f171ab17549f562331
DevGilded <98685578+DevGilded@users.noreply.github.com>
2025-10-18 10:28:13 +0800

    update control

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,25 +1,38 @@
 extends CharacterBody2D
+class_name Player
 
+@export var gravity = 350
+@export var speed = 100
+var jump_force = 60
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -300.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(delta):
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+		
+	if Input.is_action_just_pressed("ui_accept") && is_on_floor():
+		jump(jump_force)
+		
+	if velocity.y >= 500:
+		velocity.y = 500
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction != 0:
+		$Sprite2D.flip_h = (direction == -1)
+	velocity.x = direction  * speed 
 	move_and_slide()
+	#update_animations(direction)
+	
+func jump(force):
+	velocity.y = -force
+	
+	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			$AnimatedSprite2D.play("idle")
+		else:
+			$AnimatedSprite2D.play("run")
+	else:
+		if velocity.y < 0:
+			$AnimatedSprite2D.play("jump")
+		else:
+			$AnimatedSprite2D.play("fall")
